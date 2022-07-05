e07fc43b921053f0fe167bf44f19321520043dc3
higor-haider <higor.haider@sumup.com>
2022-07-05 20:38:50 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://gremio.net/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vamos tricolor!
         </a>
       </header>
     </div>

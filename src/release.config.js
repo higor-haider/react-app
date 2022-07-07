d5d06d925d98e60a0171d38baab97c2fd330f968
higor-haider <higor.haider@sumup.com>
2022-07-06 23:58:13 -0300

    fix: hotfix

--- src/release.config.js
+++ src/release.config.js
@@ -4,13 +4,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/npm",
-      {
-        npmPublish: false,
-        tarballDir: "dist",
-      },
-    ],
+    "@semantic-release/npm",
     [
       "@semantic-release/github",
       {
